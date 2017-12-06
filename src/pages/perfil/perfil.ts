dfb5aa251c4ec66a24175017e599566e361529e7
snownets <snownesst@gmail.com>
2017-12-06 16:51:22 -0600

    chales

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -5,8 +5,6 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
 import { UsuariosProvider } from '../../providers/usuarios/usuarios';
-import { MyApp } from '../../app/app.component';
-
 
 @IonicPage()
 @Component({
@@ -34,7 +32,7 @@ export class PerfilPage {
 
     this._usuarioProvider.obtener_storage().then(usr => {
 
-      this.items = afDB.list('db_users/' + usr ).valueChanges();
+      this.items = afDB.list('db_users/' + usr).valueChanges();
       loading.dismiss();
 
 
@@ -55,42 +53,39 @@ export class PerfilPage {
 
   }
 
-  logOut(){
+  logOut() {
 
     let loading = this.loadingCtrl.create({
-      
-            content: "Cargando perfil..."
-      
-          });
-      
-          loading.present();
-      
-          this._usuarioProvider.borrar_storage().then(usr => {
+
+      content: "Cerrando sesion..."
+
+    });
+
+    loading.present();
+
+    this._usuarioProvider.borrar_storage().then(usr => {
+
+      window.location.reload(false);
+      loading.dismiss();      
 
 
-            this.navCtrl.push(MyApp);
-      
-            
-            loading.dismiss();
-      
-      
-          }).catch(error => {
-      
-            loading.dismiss();
-      
-            this.toastCtrl.create({
-      
-              message: 'ERROR en obtener el perfil',
-              duration: 1500
-      
-            }).present();
-      
-            console.log("ERROR en perfil: " + JSON.stringify(error));
-      
-          });
+    }).catch(error => {
+
+      loading.dismiss();
+
+      this.toastCtrl.create({
+
+        message: 'ERROR en obtener el perfil',
+        duration: 1500
+
+      }).present();
+
+      console.log("ERROR en perfil: " + JSON.stringify(error));
+
+    });
 
   }
-  
+
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad PerfilPage');
